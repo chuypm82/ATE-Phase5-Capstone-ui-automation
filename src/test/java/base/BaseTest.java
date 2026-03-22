97de4b70916b0c150260ea612d7cea5916ead6df
Jesus Perez <chuypm@hotmail.com>
2026-03-22 16:04:25 -0400

    Update5

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -17,24 +17,22 @@ public class BaseTest {
 	@BeforeMethod
 	public void setup() {
 
-		WebDriverManager.chromedriver().setup();
-		// Add steps to open the Chrome browser and maximize the window
-		
-		ChromeOptions options = new ChromeOptions();
-		//options.addArguments("--headless=new");
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--disable-gpu");
-		options.addArguments("--window-size=1920,1080");
-		driver = new ChromeDriver(options);
-		// maximize the window
-		driver.manage().window().maximize();
-		// Add steps to open the application URL in the browser
-		driver.get("http://localhost:8080/medicare2/");
-		// Add steps to code to delete cookies
-		driver.manage().deleteAllCookies();
-		// Add Selenium commands in the PageLoadTimeout code
-		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
+        ChromeOptions options = new ChromeOptions();
+
+        // Recommended for Docker / CI environments
+        options.addArguments("--headless=new"); // Use new headless mode
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        // Selenium Manager automatically downloads and uses the correct ChromeDriver
+        driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("http://172.19.144.1:8080/medicare2/");
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 	}
 
 	@AfterMethod
